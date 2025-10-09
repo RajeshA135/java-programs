f0e582826d4ea6f5260f5e87dbc32342265fa2fc
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-09 20:39:26 +0530

    Added ArithmaticOperators

---
+++ Operators/ArithmaticOperators.java
@@ -0,0 +1,20 @@
+public class ArithmaticOperators{
+    public static void main(String args[])
+    {
+        int num1=50;
+        int num2=5;
+        int sum=num1+num2;
+        int diff=num1-num2;
+        int prod=num1*num2;
+        int div=num1/num2;
+        int mod=num1%num2;
+        System.out.println("--- Arithmetic Operations ---");
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Addition (+): " + sum);
+        System.out.println("Subtraction (-): " + diff);
+        System.out.println("Multiplication (*): " + prod);
+        System.out.println("Division (/): " + div);
+        System.out.println("Modulus (%): " + mod);
+    }
+}
