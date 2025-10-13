4d26e4455ef6f30255463b83cd5d1e8a6a3c5261
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-13 19:38:34 +0530

    Updated ternaryOperator with BiggestOfThreeNumbers

--- Operators/ternaryOperator.java
+++ Operators/ternaryOperator.java
@@ -1,10 +1,18 @@
 public class ternaryOperator {
     public static void main(String args[]){
         // condition? true block : false block
+
         int a=10;
         int b=20;
         String s=a>b? "a is big":a==b?"a & b are Equal":"b is big";
         System.out.println(s);
+        //biggest of three numbers
+        int x=100;
+        int y=1000;
+        int z=10;
+        String s1=x>y?(x>z?"x is big":"z is big")
+                     :(y>z?"y is big":"z is big");
+        System.out.println(s1);
 
     }
 
