3d78e20b55f41a4a08c1b98d8c9503540830adbf
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-13 19:27:01 +0530

    Added TernaryOperator

--- Operators/shiftOperator.java
+++ Operators/shiftOperator.java
@@ -3,7 +3,7 @@ public class shiftOperator{
         //left shift << --> a*2^n
         //right shift >> --> a/2^n
         int a=20>>2;
-        int b=13>>3;
+        int b=13<<3;
         System.out.println(a);
         System.out.println(b);
     }
---
+++ Operators/ternaryOperator.java
@@ -0,0 +1,12 @@
+public class ternaryOperator {
+    public static void main(String args[]){
+        // condition? true block : false block
+        int a=10;
+        int b=20;
+        String s=a>b? "a is big":a==b?"a & b are Equal":"b is big";
+        System.out.println(s);
+
+    }
+
+
+}
