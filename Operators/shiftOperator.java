71e4b29e57e61918d69e499205b8a5e6e5542142
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-13 19:02:27 +0530

    Added ShiftOperators

---
+++ Operators/shiftOperator.java
@@ -0,0 +1,10 @@
+public class shiftOperator{
+    public static void main(String args[]){
+        //left shift << --> a*2^n
+        //right shift >> --> a/2^n
+        int a=20>>2;
+        int b=13>>3;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
