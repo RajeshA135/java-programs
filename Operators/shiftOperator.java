02f8d7533a5eb93938aae7f0c905e081fa234643
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-13 19:42:35 +0530

    Updated ShiftOperators

--- Operators/shiftOperator.java
+++ Operators/shiftOperator.java
@@ -2,8 +2,8 @@ public class shiftOperator{
     public static void main(String args[]){
         //left shift << --> a*2^n
         //right shift >> --> a/2^n
-        int a=20>>2;
-        int b=13<<3;
+        int a=20>>2;//5
+        int b=13<<3;//104
         System.out.println(a);
         System.out.println(b);
     }
