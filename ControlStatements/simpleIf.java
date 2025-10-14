1b89b5c94b4b50dedbbdfbfdd80514c4fca96f41
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-14 16:24:26 +0530

    Added ControlStatements & simpleIf

---
+++ ControlStatements/simpleIf.java
@@ -0,0 +1,7 @@
+public class simpleIf{
+    public static void main(String args[]){
+        int age = 55;
+        if(age>=18)System.out.println("Elegible for Vote");
+        else System.out.println("Not Elegible for Vote");
+    }
+}
