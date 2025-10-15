90906d32e0cd4b4774233ea4470746c8a6984149
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-15 19:48:43 +0530

    Added forLoop

---
+++ Looping/forLoop.java
@@ -0,0 +1,8 @@
+public class forLoop {
+    public static void main(String[] args) {
+        // print numbers from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
