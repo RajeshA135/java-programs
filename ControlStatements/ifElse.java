d75252d806682cc0c32798da1b93165bd169fc2d
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-15 18:24:30 +0530

    Added ifElse

---
+++ ControlStatements/ifElse.java
@@ -0,0 +1,9 @@
+public class ifElse{
+    public static void main(String args[]){
+    //Vote Eligibility
+        int voterAge=32;
+        int votingAge=18;
+        if(voterAge>=18)System.out.println("Voter is Eligible for Vote...");
+        else System.out.println("Voter is not Eligible for vote");
+    }
+}
