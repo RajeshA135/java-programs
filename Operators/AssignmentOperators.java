27a60e48e437853be0d94d6fda2f3ecd8e988d28
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-15 17:59:36 +0530

    Added AssignmentOperators

---
+++ Operators/AssignmentOperators.java
@@ -0,0 +1,17 @@
+public class AssignmentOperators {
+    public static void main(String args[]){
+        //Assignment Operators are (=,+=,-=,/=,%=)
+        int a=30;
+        int b=20;
+        a+=b;//here a=50
+        System.out.println(a);
+        a-=b;//here a=30
+        System.out.println(a);
+        a*=b;//here a=60
+        System.out.println(a);
+        a/=b;//here a=30
+        System.out.println(a);
+        a%=b;//here a=10
+        System.out.println(a);
+    }
+}
