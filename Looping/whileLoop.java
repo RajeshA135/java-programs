8695d8296f0cf0dde0189a4297bf0986b0d3e2bc
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-16 18:27:25 +0530

    Added whileLoop

---
+++ Looping/whileLoop.java
@@ -0,0 +1,11 @@
+public class whileLoop{
+    public static void main(){
+        int n=150;
+        int c=0;
+        while(n>0){
+           c++;
+           n=n/10;
+        }
+        System.out.println(c);
+    }
+}
