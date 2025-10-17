7f5f2374204b8b6a94f3c9d297158c6449480902
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-17 15:21:18 +0530

    Added breakExample

---
+++ Looping/breakExample.java
@@ -0,0 +1,10 @@
+public class breakExample{
+    public static void main(){
+        for(int i=1; i<=10; i++){
+            if(i==3){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
