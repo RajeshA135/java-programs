5ebbbf63f4ac319cb62b9a56eb3194fdd7a5ff4c
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-17 15:25:26 +0530

    Added continueExample

---
+++ Looping/continueExample.java
@@ -0,0 +1,10 @@
+public class continueExample{
+    public static void main(){
+        for(int i=1; i<=10; i++){
+            if(i==3){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
