a50ea25ca91c76b4c44812d2209eb91b58ab6de6
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-18 14:25:29 +0530

    Added switchCase

---
+++ ControlStatements/switchCase.java
@@ -0,0 +1,14 @@
+public class switchCase{
+public static void main(String args[]){
+int a=1;
+switch(a){
+    case 1 : System.out.println("Hi");
+                break;
+    case 2 : System.out.println("Hello");
+                break;
+    case 3 : System.out.println("welcome");
+                break;
+    default: System.out.println("Byee");
+}
+}
+}
