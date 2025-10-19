9478ae2ca4f7e3af5ba810f948aaf9eff4e0216a
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-19 22:41:21 +0530

    Added UserInput file

---
+++ Basics/UserInput.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class UserInput{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        System.out.println("Sum of a & b is: "+(a+b));
+
+    }
+}
