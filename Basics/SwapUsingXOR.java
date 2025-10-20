02f5b1df054e1d86602d28227fe28ba8bb7e9867
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-20 16:33:47 +0530

    Added SwapUsingXOR

---
+++ Basics/SwapUsingXOR.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SwapUsingXOR {
+    public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter a & b :");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println("Before Swapping a:"+a+" and b:" +b);
+        a=a ^ b;
+        b=a ^ b;
+        a=a ^ b;
+        System.out.println("After Swapping a:"+a+" and b:" +b);
+    }
+}
