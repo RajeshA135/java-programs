6da3dafb620f84d307ccd39404b455cba2f7086c
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-23 18:38:39 +0530

    Added LocalVariable Example

---
+++ Basics/LocalVariable.java
@@ -0,0 +1,8 @@
+//Create a Java Application where we have one class, it contains one local variable in the main method. display the value of the local variable on the console by using printing statements.
+
+class Local{
+	public static void main(String[] args){
+		int local_variable=30;
+	System.out.println(local_variable);
+	}	
+}
