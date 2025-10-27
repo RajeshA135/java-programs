65b1b7f18de413e197199f22024273f616f5a11b
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-27 15:02:57 +0530

    Added Method Example-10

---
+++ Methods/q10.java
@@ -0,0 +1,22 @@
+//10. Create a Java Application where we have one class, it contains one non-static method m1 with parameter int and returns a Boolean value, and a static method m2 with parameter float and returns a character value, then invoke both the methods under the main method by providing dynamic inputs.
+
+import java.util.Scanner;
+class Example{
+	boolean m1(int i){
+		return true;
+		}
+	static char m2(float f){
+		return 'A';
+		}
+		public static void main(String[] args){
+			Scanner sc= new Scanner(System.in);
+			int a=sc.nextInt();			
+			float f = sc.nextFloat();
+
+			
+			Example obj = new Example();
+			System.out.println(obj.m1(a));
+			System.out.println(Example.m2(f));
+			
+			}
+	}
