88cd92a9ed7085c47c6eee64124eadaa1cb857f4
Rajesh Allam <rajeshallam9966@gmail.com>
2025-10-29 11:33:29 +0530

    Added Method Example-17

---
+++ Methods/q17.java
@@ -0,0 +1,20 @@
+//17.	create a java application where we have one class, it contains methods like add having two parameters as integers, it is going returns the value as the addition of two formal parameters, method sub having two parameters as integers and it is going to return the value as subtraction of two parameters, method multi which doesn't have any parameters and it has to return the value as multiplication of both ‘add’ and ‘sub’ method results. then invoke multi under the main method by providing input values dynamically. (P.java)
+
+import java.util.Scanner;
+class Example{
+	Scanner sc= new Scanner(System.in);
+	int add(int num1, int num2){
+		return num1+num2;
+		}
+	int sub(int num1, int num2){
+		return num1-num2;
+		}
+	int multi(){
+		int result= add(sc.nextInt(),sc.nextInt()) * sub(sc.nextInt(),sc.nextInt());
+		return result; 
+		}
+	public static void main(String args[]){
+			Example e =  new Example();
+		System.out.println("Addition of  two numbers * Subtraction of two numbers is:  "+e.multi());
+		}
+	}
