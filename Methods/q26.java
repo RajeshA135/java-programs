912c09d6dc397af34916eab9c83c9359f746ee86
Rajesh Allam <rajeshallam9966@gmail.com>
2025-11-02 20:31:43 +0530

    Added Method Example-26

---
+++ Methods/q26.java
@@ -0,0 +1,18 @@
+//26.	Create a Java Application where we have one class it contains one static method m1 having no parameters and returns a Class variable, nonstatic method m2 having a parameter boolean and returns nothing, then invoke both methods under the main by providing dynamic inputs and without creating an object or without using object reference directly.
+
+import java.util.Scanner;
+	class Example{
+		static Scanner sc = new Scanner(System.in);
+		static Example e = new Example();
+		int a=sc.nextInt();
+		static int m1(){
+		return e.a;
+			}
+		void m2(boolean b){
+		System.out.println(b);
+		}
+		public static void main(String [] args){
+			System.out.println(m1());
+			e.m2(sc.nextBoolean());
+		}
+	}
