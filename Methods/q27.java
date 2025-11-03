aac2b0497afd1219978e49007180c0e98fc59402
Rajesh Allam <rajeshallam9966@gmail.com>
2025-11-03 12:54:47 +0530

    Modified Method Example-27

--- Methods/q27.java
+++ Methods/q27.java
@@ -1,14 +1,14 @@
 //27.	Create a Java Application where we have one class, it contains one static method m1 having parameter class variable and returns nothing, then invoke this method under main by providing dynamic inputs.
 import java.util.Scanner;
 class Example{
-	Scanner sc = new Scanner(System.in);
-	static Example e = new Example();
-	int a = sc.nextInt();
-	static void m1(){
-		System.out.println(e.a);
+	static Scanner sc = new Scanner(System.in);
+
+	static int a = sc.nextInt();
+	static void m1(int a){
+		System.out.println(a);
 	}
 	public static void main(String[] args){
-		e.m1();
+		Example.m1(a);
 		
 	}
 }
